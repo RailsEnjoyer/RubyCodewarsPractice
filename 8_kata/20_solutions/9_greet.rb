85a7bc62e190d33fa21d9152cefcfeb913683d3c
railsenjoyer <railsenjoyer@gmail.com>
2024-03-15 20:07:55 +0300

    completed 9 tasks

---
+++ 8_kata/20_solutions/9_greet.rb
@@ -0,0 +1,5 @@
+# Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+def greet(name,owner) 
+  name==owner ? 'Hello boss' : 'Hello guest'
+end
