fd660b64070fee065c0151ba89493bf80cff3d08
railsenjoyer <railsenjoyer@gmail.com>
2024-03-27 21:41:22 +0300

    added 29 solution

---
+++ 8_kata/40_solutions/29_abbrev_name.rb
@@ -0,0 +1,10 @@
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+# The output should be two capital letters with a dot separating them.
+
+# It should look like this:
+
+
+def abbrev_name(name)
+ name.split.map{ |a| a[0]}.join(".").upcase
+end
