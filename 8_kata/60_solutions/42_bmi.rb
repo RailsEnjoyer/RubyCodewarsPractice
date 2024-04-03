fbceb9b3d178406ee363480e7c04779694bafb1c
railsenjoyer <railsenjoyer@gmail.com>
2024-04-03 21:23:10 +0300

    added 42 solution

---
+++ 8_kata/60_solutions/42_bmi.rb
@@ -0,0 +1,14 @@
+# Write function bmi that calculates body mass index (bmi = weight / height2).
+
+# if bmi <= 18.5 return "Underweight"
+
+# if bmi <= 25.0 return "Normal"
+
+# if bmi <= 30.0 return "Overweight"
+
+# if bmi > 30 return "Obese"
+
+def bmi(weight, height)
+  bmi = weight/(height*height)
+  bmi <= 18.5 ? "Underweight" : bmi <= 25.0 ? "Normal" : bmi <= 30.0 ? "Overweight" : bmi > 30 ? "Obese" : false
+end
