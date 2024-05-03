7dc4dcf791bfd0a1597d49816738e52a73ce4ec8
railsenjoyer <railsenjoyer@gmail.com>
2024-05-03 23:28:55 +0300

    added 95 solution

---
+++ 8_kata/100_solutions/95_total_goals.rb
@@ -0,0 +1,9 @@
+
+# Messi's Goal Total
+# Use variables to find the sum of the goals Messi scored in 3 competitions
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = [la_liga_goals, champions_league_goals, copa_del_rey_goals].inject(:+)
