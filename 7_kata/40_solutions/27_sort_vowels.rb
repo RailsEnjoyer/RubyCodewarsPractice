cf7f47a3547ba41a4ae9d8dd267423d719e17365
railsenjoyer <railsenjoyer@gmail.com>
2024-05-20 23:42:13 +0300

    added 2 solution

---
+++ 7_kata/40_solutions/27_sort_vowels.rb
@@ -0,0 +1,26 @@
+# Notes:
+
+# List all the Vowels on the right side of |
+# List every character except Vowels on the left side of |
+# for the purpose of this kata, the vowels are : a e i o u
+# Return every character in its original case
+# Each line is seperated with \n
+# Invalid input ( undefined / null / integer ) should return an empty string
+
+# Write a function which takes a input string s and return a string in the following way:
+
+
+def sort_vowels(s)
+  return '' unless s.is_a?(String) && !s.empty?
+
+  vowels = ["a", "e", "i", "o", "u"]
+  result = s.chars.map do |char|
+    if vowels.include?(char.downcase)
+      "|" + char 
+    else
+      char + "|"
+    end
+  end
+
+  result.join("\n")
+end
