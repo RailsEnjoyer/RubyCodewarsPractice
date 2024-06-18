0d6bd17953422966b2d4264ecf5538e069a71556
railsenjoyer <railsenjoyer@gmail.com>
2024-06-18 14:57:42 +0300

    added 75 solution

---
+++ 7_kata/80 solutions/75_capitalize.rb
@@ -0,0 +1,18 @@
+# Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+# For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+# The input will be a lowercase string with no spaces.
+
+# Good luck!
+
+# If you like this Kata, please try:
+
+# Indexed capitalization
+
+# Even-odd disparity
+
+def capitalize(s)
+ string = s.chars.map.with_index { |n, i| i % 2 == 0 ? n.capitalize : n}.join
+ [string, string.swapcase]
+end
